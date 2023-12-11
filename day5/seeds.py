e601436b5fe79246c829d2d2dd2522d58703aed0
dylang23 <galarza.danielemanuel96@gmail.com>
2023-12-11 07:47:42 -0300

    day 5 finished

--- day5/seeds.py
+++ day5/seeds.py
@@ -1,3 +1,6 @@
+from sys import maxsize
+from numpy import arange
+
 def input_file(file):
 
     with open(file, 'r') as file_input:
@@ -9,7 +12,6 @@ def input_file(file):
     
     lines = lines[3:]
     maps = []
-    
     
     for _ in range(7):
         
@@ -31,7 +33,6 @@ def input_file(file):
     
             
 def convert(maps, number, category_id):
-    
     
     for list_number in maps[category_id]:
         source_range_start = list_number[1]
@@ -58,7 +59,62 @@ def seeds(file):
     return min(numbers_convert)
     
     
-    
+def seeds_part_two(file):
+    seeds , maps = input_file(file)
 
-print(seeds('input_test.txt'))
-print(seeds('input.txt'))
+    seeds_range = [(seeds[i], seeds[i + 1]) for i in range(0, len(seeds), 2)]  
+
+    lowest_location = maxsize
+    for seed_range in seeds_range:
+
+        seed = seed_range[0]
+        seed_final = seed_range[0] + seed_range[1]
+        while seed < seed_final:
+            number_convert = seed
+            
+            salto = maxsize
+            for j in range(7):
+                 flag = False
+                 for list_number in maps[j]:
+                    source_range_start = list_number[1]
+                    range_length = list_number[2]
+                    if number_convert >= source_range_start and number_convert <= source_range_start + (range_length - 1):
+                        salto = min(salto, source_range_start + (range_length - 1) - number_convert)
+                        number_convert = list_number[0] + abs(number_convert - source_range_start)
+                        # print(j, seed, number_convert) 
+                        flag = True
+                        break
+                 if not flag:
+                    pass
+                    #  print(j, seed, number_convert)   
+            print(seed, salto)
+            seed += salto + 1
+            lowest_location = min(lowest_location, number_convert)
+
+    return lowest_location
+
+
+    # print(seeds_range)
+
+
+# TEST PARA LA PRIMERA PARTE
+
+# print(seeds('input_test.txt'))
+# print(seeds('input.txt'))
+
+
+
+# TEST PARA LA SEGUNDA PARTE
+
+# print(seeds_part_two('input_test.txt'))
+#print(seeds_part_two('input_test.txt'), seeds_part_two('input_test.txt') == 46, 46)
+print(seeds_part_two('input.txt'), 11611182)
+
+
+
+
+
+
+
+
+
